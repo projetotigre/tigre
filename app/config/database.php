f38f3931a4beef13538766223af2de3d221dacaa
Gabriel Vitor <gvsrepins@gmail.com>
2014-05-24 13:34:29 -0300

    add dev db config

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
+			'database'  => 'dbname',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => '123',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
