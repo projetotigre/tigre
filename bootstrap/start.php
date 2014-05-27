32bd73ae3909eb73de7f8cfd0c10e585623a29b4
Gabriel Vitor <gvsrepins@gmail.com>
2014-05-27 02:56:28 -0300

    add config dinamic env

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -24,11 +24,11 @@ $app = new Illuminate\Foundation\Application;
 |
 */
 
-$env = $app->detectEnvironment(array(
+$env = $app->detectEnvironment(function(){
 
-	'local' => array('your-machine-name'),
+    return getenv('APP_ENV') ?: 'local';
 
-));
+});
 
 /*
 |--------------------------------------------------------------------------
